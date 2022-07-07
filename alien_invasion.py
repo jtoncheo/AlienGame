5b6ab8c077702d890204d9c3ac459a54daeb1655
Jesus Tonche <jesustoncheochoa@gmail.com>
2022-07-07 15:31:53 -0500

    Adding bullet functionality

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion: 
     ''' Overall Class to manage game assets and behavior.'''
@@ -20,6 +21,8 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+
 
     def run_game(self):
         '''Start the main loop for the game.'''
@@ -27,6 +30,14 @@ class AlienInvasion:
         while True: 
             self._check_events()
             self.ship.update()
+            self.bullets.update()
+
+            #Get rid of bullets that have disappeared.
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0 : 
+                    self.bullets.remove(bullet)
+            print(len(self.bullets))
+
             self._update_screen()
 
     def _check_events(self):
@@ -48,6 +59,9 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+            print('Fired a bullet')
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
@@ -58,8 +72,15 @@ class AlienInvasion:
         '''Update images on the screen, and flip to the new screen.'''
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         pygame.display.flip()
+
+    def _fire_bullet(self):
+        '''Create a neew bullet and add it to the bullets group.'''
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
 if __name__ == '__main__':
     # Make a game instance, and run the game. 
