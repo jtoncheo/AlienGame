57654b93e927e5ae775b5193f89b9f22f74d8967
Jesus Tonche <jesustoncheochoa@gmail.com>
2022-07-07 18:45:57 -0500

    Limiting # of Bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,14 +30,7 @@ class AlienInvasion:
         while True: 
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-
-            #Get rid of bullets that have disappeared.
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0 : 
-                    self.bullets.remove(bullet)
-            print(len(self.bullets))
-
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -61,7 +54,6 @@ class AlienInvasion:
             sys.exit()
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
-            print('Fired a bullet')
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
@@ -79,8 +71,19 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         '''Create a neew bullet and add it to the bullets group.'''
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        '''Update the positio of bullets and get rid of old bullets.'''
+        #update bullet positions.
+        self.bullets.update()
+
+        #Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0 : 
+                self.bullets.remove(bullet)
 
 if __name__ == '__main__':
     # Make a game instance, and run the game. 
